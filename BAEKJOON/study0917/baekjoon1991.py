8b2e5d211caaa5d849d0a2e1f96b756efb4dd451
LEE SY <tkdduq3546@naver.com>
2021-09-15 23:54:36 +0900

    0915TIL2

---
+++ BAEKJOON/study0917/baekjoon1991.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+# 전위 순회
+def pre_order(start):
+    if start != '.':
+        print(start, end='')
+        pre_order(tree[start][0])
+        pre_order(tree[start][1])
+
+# 중위 순회
+def in_order(start):
+    if start != '.':
+        in_order(tree[start][0])
+        print(start, end='')
+        in_order(tree[start][1])
+
+# 후위 순회
+def post_order(start):
+    if start != '.':
+        post_order(tree[start][0])
+        post_order(tree[start][1])
+        print(start, end='')
+
+
+
+N = int(input())
+tree = {}
+
+for _ in range(N):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+pre_order('A')
+print()
+in_order('A')
+print()
+post_order('A')
