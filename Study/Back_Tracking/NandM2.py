0133b73094a7115ca0607031752c9d4eaf5e4f67
LEE SY <tkdduq3546@naver.com>
2021-10-30 23:58:11 +0900

    N&M2 수정

--- Study/Back_Tracking/NandM2.py
+++ Study/Back_Tracking/NandM2.py
@@ -1,16 +1,22 @@
-def N_and_M(sel_idx, arr_idx):
-    if sel_idx == M:
-        print(*sel)
-    else:
-        for idx in range(arr_idx, N - M + sel_idx + 1):
-            if not visited[idx]:
-                visited[idx] = 1
-                sel[sel_idx] = arr[idx]
-                N_and_M(sel_idx+1, arr_idx+1)
-                visited[idx] = 0
+def N_and_M(idx):
+    if idx == M:
+        tmp = sorted(arr)
+        if tmp in double_check:
+            return
+        double_check.append(tmp)
+        print(*tmp)
+        return
+
+    for i in range(N):
+        if not used[i]:
+            used[i] = 1
+            arr[idx] = i + 1
+            N_and_M(idx+1)
+            used[i] = 0
 
 N, M = map(int, input().split())
-arr = [0 for _ in range(N)]
+arr = [0 for _ in range(M)]
+used = [0] * N
+double_check = []
 
-
-N_and_M(0, 0)
+N_and_M(0)
