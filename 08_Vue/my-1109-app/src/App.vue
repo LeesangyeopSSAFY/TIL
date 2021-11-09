39ea5babd4c662831647db74fbfe638bd98b8b4e
LEE SY <tkdduq3546@naver.com>
2021-11-09 22:06:44 +0900

    1109Vue TIL

---
+++ 08_Vue/my-1109-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <todo-list-input @add-todo="onAddTodo"></todo-list-input>
+  </div>
+</template>
+
+<script>
+import TodoListInput from './components/TodoListInput.vue'
+
+export default {
+  name: 'MyTodoList',
+  components: {
+    TodoListInput
+  },
+  methods: {
+    onAddTodo: function (todoData) {
+      console.log(todoData)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
