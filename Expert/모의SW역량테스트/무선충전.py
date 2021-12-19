51164336a3e31c7b5d508a4aa8c2d1b52116beba
LEE SY <tkdduq3546@naver.com>
2021-12-19 23:10:02 +0900

    1219TIL

---
+++ Expert/모의SW역량테스트/무선충전.py
@@ -0,0 +1,7 @@
+T = int(input())
+for t in range(1, T+1):
+    M, A = map(int, input().split())
+    userA = list(map(int, input().split()))
+    userB = list(map(int, input().split()))
+    AP_list = [list(map(int, input().split())) for _ in range(A)]
+    
