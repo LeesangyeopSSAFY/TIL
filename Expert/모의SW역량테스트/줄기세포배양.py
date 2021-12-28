d0d69af45bb589893bd3a4fc0c3bf487454c2243
LEE SY <tkdduq3546@naver.com>
2021-12-28 23:39:10 +0900

    줄기세포배양

--- Expert/모의SW역량테스트/줄기세포배양.py
+++ Expert/모의SW역량테스트/줄기세포배양.py
@@ -4,43 +4,55 @@ dc = [0, 0, -1, 1]
 T = int(input())
 for t in range(1, T+1):
     N, M, K = map(int, input().split())
-    board = [[0]*(M+K*2) for _ in range(N+K*2)]
-    first_status = [list(map(int, input().split())) for _ in range(N)]
+    live = [[0]*(M + K*2 + 2) for _ in range(N + K*2 + 2)]
+    check = [[0]*(M + K*2 + 2) for _ in range(N + K*2 + 2)]
     for i in range(N):
+        data = list(map(int, input().split()))
         for j in range(M):
-            if first_status[i][j] > 0:
-                X = first_status[i][j]
-                board[K+i][K+j] = [X, X, X, 0]
+            live[i][j] = data[j]
+            if data[j] > 0:
+                check[i][j] = -(data[j])
     
-    for k in range(K+1):
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if type(board[r][c]) == list:
-                    if board[r][c][0] > 0 and board[r][c][3] != k: # 비활성화 상태이면
-                        board[r][c][0] -= 1
-                    elif board[r][c][0] == 0 and board[r][c][1] > 0: #비활성 상태가 끝나고 활성 상태라면
-                        vitality = board[r][c][2] # 번식하는 줄기 세포의 생명력 수치
-                        board[r][c][1] -= 1
-                        for dir in range(4):
-                            nr = r + dr[dir]
-                            nc = c + dc[dir]
-                            if board[nr][nc] == 0: # 줄기세포가 없는 셀이라면
-                                board[nr][nc] = [vitality, vitality, vitality, k]
-                            else:
-                                # if board[nr][nc][0] == board[nr][nc][1] == board[nr][nc][2] and vitality > board[nr][nc][2]: # 동시에 번식하는 줄기 세포 중 생명력 수치가 더 낮은 경우
-                                if board[r][c][3] == k and vitality > board[nr][nc][2]: # 동시에 번식하는 줄기 세포 중 생명력 수치가 더 낮은 경우
-                                    board[nr][nc] = [vitality, vitality, vitality, k]
-
+    for _ in range(K):
+        board = [[0]*(M+K*2+2) for _ in range(N+K*2+2)]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if live[i][j] == 0 or live[i][j] == -1:
+                    continue
+                if check[i][j] < 0:
+                    if check[i][j] == -1:
+                        check[i][j] = live[i][j]
+                    else:
+                        check[i][j] += 1
+                elif check[i][j] > 0:
+                    r, c = i, j
+                    for dir in range(4):
+                        nr = (r+dr[dir]) % (N+K*2+2)
+                        nc = (c+dc[dir]) % (M+K*2+2)
+                        if live[nr][nc] == -1 or live[nr][nc] > 0:
+                            continue
+                        if board[nr][nc] == 0:
+                            board[nr][nc] = live[r][c]
+                            check[nr][nc] = -live[r][c]
+                        elif board[nr][nc] < live[r][c]:
+                            board[nr][nc] = live[r][c]
+                            check[nr][nc] = -live[r][c]
+                    if check[i][j] == 1:
+                        live[i][j] = -1
+                        check[i][j] = 0
+                    else:
+                        check[i][j] -= 1
+        
+        for i in range(N+K*2+2):
+            for j in range(M+K*2+2):
+                if board[i][j] > 0 and live[i][j] == 0:
+                    live[i][j] = board[i][j]
+    
     ans = 0
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if type(board[i][j]) == list:
-                if board[i][j][1] > 0:
-                    ans += 1
+    for i in range(N+K*2+2):
+        for j in range(M+K*2+2):
+            if check[i][j] != 0:
+                ans += 1
     
     print('#{} {}'.format(t, ans))
-    # print(board)
-
-    
-    
 
